411cbd424fa89eca19929dfad98190b095926da2
echyarrisandi <>
2022-11-30 17:05:47 +0700

    PERBAIKI LAGI RANI

---
+++ app/src/main/java/com/syahranipab/Activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.syahranipab.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.syahranipab.dailyquote.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/syahranipab/Model/QuoteModel.java
@@ -0,0 +1,13 @@
+package com.syahranipab.Model;
+
+public class QuoteModel {
+    private String text, author;
+
+    public String getText() {
+        return text;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
